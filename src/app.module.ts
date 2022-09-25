db5b3680eaeffc54a241373ffaa73bdbbec44ff1
daniil-kh <daniil.khmurovich@gmail.com>
2022-09-25 23:33:55 +0300

    feat: merge docker-compose files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import {SequelizeModule} from '@nestjs/sequelize'
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
