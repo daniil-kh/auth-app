a37a4d0d0e70e2449a5ab4cf7b7036b189ccea09
daniil-kh <daniil.khmurovich@gmail.com>
2022-10-02 17:05:00 +0300

    fix: refactor user-files service typing

--- src/user-files/infrastructure/repositories-interfaces/user-file.repository-interface.ts
+++ src/user-files/infrastructure/repositories-interfaces/user-file.repository-interface.ts
@@ -1,5 +1,19 @@
+import { QueryOptionsWithType, QueryTypes } from 'sequelize';
+
 export interface UserFileCreationAttributes {
   name: string;
   url: string;
   userId: string;
 }
+
+export interface UserFileDto {
+  id: string;
+  name: string;
+  url: string;
+  userId: string;
+}
+
+export type TypedQueryParams<T extends QueryTypes = QueryTypes.SELECT> = Omit<
+  QueryOptionsWithType<T>,
+  'type'
+>;
